3addba7b36b16c937f2d7679b1231bf4bd8dc635
cq_xifan <175510896@qq.com>
2026-05-22 16:55:53 +0800

    重庆招考
    dd

---
+++ cqksy/namespace.ts
@@ -0,0 +1,11 @@
+// 文件路径: lib/routes/cqksy/namespace.ts
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '重庆市教育考试院',
+    url: 'cqksy.cn',
+    description: `重庆招考提供权威的考试信息，涵盖普通高考、研究生考试、成人高考等众多领域。`,
+    zh: {
+        name: '重庆招考',
+    },
+};
